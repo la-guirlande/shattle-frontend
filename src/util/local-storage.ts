6dd72580232cb897672829a73d13d583354475bf
Rocknologie <rocknologie@hotmail.fr>
2021-06-01 15:45:58 +0200

    Query hooks

---
+++ src/util/local-storage.ts
@@ -0,0 +1,12 @@
+/**
+ * Local storage key enumeration.
+ * 
+ * This enum is used to store / retrieve local storage data with typed keys.
+ */
+export enum LocalStorageKey {
+    ACCESS_TOKEN = 'access-token',
+    REFRESH_TOKEN = 'refresh-token',
+    CURRENT_PARTY_ID = 'current-party-id',
+    CURRENT_PARTY_CODE = 'current-party-code',
+    CURRENT_PARTY_AUTHOR_NAME = 'current-party-author-name'
+}
