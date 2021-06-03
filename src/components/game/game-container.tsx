1347fa6bd44fd14a399ee8cec0d6c714e1e16f57
Lelberto <jsurieux@gmail.com>
2021-06-03 16:04:30 +0200

    Add game.start event (WIP)

--- src/components/game/game-container.tsx
+++ src/components/game/game-container.tsx
@@ -1,6 +1,7 @@
 import { FC, useContext, useEffect, useRef, useState } from 'react';
 import { Status, useQuery } from '../../hooks/query-hooks';
 import { Config } from '../../util/config';
+import { LocalStorageKey } from '../../util/local-storage';
 import { GameData } from '../../util/types/data-types';
 import { GameResponse } from '../../util/types/response-types';
 import { AuthenticationContext } from '../contexts/authentication-context';
@@ -19,9 +20,12 @@ export const GameContainer: FC = () => {
       socket.on('game.join', ({ gameId }) => {
         gameQuery.get(`${Config.API_URL}/games/${gameId}`);
       });
+      socket.on('game.start', console.log);
       socket.on('error', console.error);
       return () => {
         socket.off('game.join');
+        socket.off('game.start');
+        socket.off('error');
       }
     }
   }, [socket]);
@@ -39,13 +43,18 @@ export const GameContainer: FC = () => {
   }, [gameQuery.status]);
 
   const handleJoinGame = () => {
-    socket.emit('game.join', { code: inputRef.current.value, userId: authUser.id });
+    socket.emit('game.join', { code: inputRef.current.value, accessToken: localStorage.getItem(LocalStorageKey.ACCESS_TOKEN) });
+  }
+
+  const handleStartGame = () => {
+    socket.emit('game.start', { userId: authUser.id, gameId: game.id });
   }
 
   return (
     <div>
       <input ref={inputRef} type="text" placeholder="Game code" />
       <button onClick={handleJoinGame}>Join game</button>
+      <button onClick={handleStartGame}>Start game</button>
       {game != null && <GameCanvas map={game?.map} width={game?.map?.config?.width * game?.map?.config?.tilewidth} height={game?.map?.config?.height * game?.map?.config?.tileheight} />}
     </div>
   );
