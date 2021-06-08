d8a807b5bca78d505ab78672b347df41cc3c925f
Rocknologie <rocknologie@hotmail.fr>
2021-06-08 23:45:45 +0200

    Add config tailwind | picture & font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,22 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                primary: ['"Iceland"'],
+                iceland: ['"iceland"'],
                 secondary: [],
-
-            }
+            },
+            colors: {
+                primary: '#523295',
+                secondary: '#D975B9',
+                light: '#F4E2CA',
+                blue: {
+                    light: '#1A225C',
+                    Dark: '#0A123C'
+                }
+            },
+            // backgroundImage: {
+            //     'logo-play': "url('assets/img/jouer.png')",
+            //     'logo-shattle': "url('assets/img/shattle.png')"
+            // }
         },
     },
     variants: {
