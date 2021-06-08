26aaa5d19ae5a37a94bbcab2ff5c1e6058959879
Rocknologie <rocknologie@hotmail.fr>
2021-06-08 23:46:45 +0200

    remove unuse file

--- src/pages/home-page.tsx
+++
@@ -1,8 +0,0 @@
-export const HomePage: React.FC = () => (
-    <div className='bg-red-500'>
-        <h1 className="text-red-600">Shattle</h1>
-        <a href='/createGame'>Créer une partie</a>
-        <br></br>
-        <a href='/joinGame'>Rejoindre une partie</a>
-    </div>
-)
--- src/pages/join-game-page.tsx
+++
@@ -1,5 +0,0 @@
-export const JoinGamePage: React.FC = () => (
-    <>
-        <h1 className="text-red-600">Rejoindre une partie</h1>
-    </>
-)
