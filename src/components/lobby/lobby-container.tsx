107cad85c8b6593ca4d49c5f62236a0b0f7e2196
Rocknologie <rocknologie@hotmail.fr>
2021-06-10 17:31:13 +0200

    Lobby

--- src/components/lobby/lobby-container.tsx
+++ src/components/lobby/lobby-container.tsx
@@ -3,19 +3,18 @@ import { AuthenticationContext } from "../contexts/authentication-context";
 import { Link } from 'react-router-dom';
 
 export const LobbyContainer: React.FC = () => {
-    const { authUser } = useContext(AuthenticationContext);
 
     return (
-        <div className='bg-gradient-to-b from-secondary to-primary h-screen'>
+        <div className='bg-gradient-to-b from-secondary to-primary h-screen pt-10'>
             {/* Top's section */}
             <div className='bg-map w-5/6 h-3/6 mx-auto rounded-2xl' id='map'>
                 {/* Title */}
-                <div className='flex justify-center pt-20'>
+                <div className='flex justify-center pt-28 lg:pt-52'>
                     <img alt='' className='w-8/12' src='/img/shattle.png' />
                 </div>
 
                 {/* Play button */}
-                <div className='flex justify-center pt-16 md'>
+                <div className='flex justify-center pt-12 lg:pt-24 space-y-0'>
                     <div className='bg-white bg-opacity-50 border-2 border-white rounded-lg'>
                         <Link to='/game'>
                             <img className='w-36 lg:w-72 p-1' src='/img/jouer.png' />
@@ -24,12 +23,15 @@ export const LobbyContainer: React.FC = () => {
                 </div>
             </div>
 
-            <div className='font-iceland w-auto text-md lg:text-3xl'>Vos parties en cours</div>
-            {/* <p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p>
-            <p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p>
-            <p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p><p className='font-iceland w-auto bg-primary text-md lg:text-lg'>Vos parties en cours</p> */}
-
-            {/* List of ongoing games */}
+            <div className='divide-y-4 divide-gray-400 divide-opacity-50 pt-4 px-10 lg:px-20'>
+                <div className='font-iceland text-white text-md lg:text-4xl w-auto'>
+                    Vos parties en cours
+                </div>
+                {/* List of the user's current games */}
+                {
+                    <div></div>
+                }
+            </div>
         </div>
     )
 }
