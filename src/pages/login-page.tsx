86bd4da6856dfee1fb89e9a993591bf54a9047ed
Rocknologie <rocknologie@hotmail.fr>
2021-06-12 18:42:50 +0200

    Add login page

---
+++ src/pages/login-page.tsx
@@ -0,0 +1,7 @@
+import { AuthenticationContainer } from "../components/authentication/authentication-container";
+
+export const LoginPage: React.FC = () => (
+    <div className='bg-map'>
+        <AuthenticationContainer />
+    </div>
+)
