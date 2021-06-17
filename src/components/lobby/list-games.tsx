6ddf6c918de3b5b03479f69ed6607a416f9955a5
Rocknologie <rocknologie@hotmail.fr>
2021-06-17 14:26:08 +0200

    User's game list component

---
+++ src/components/lobby/list-games.tsx
@@ -0,0 +1,28 @@
+import { GameData } from "../../util/types/data-types"
+
+export interface ListGamesProps {
+    games?: GameData[];
+    launchGame(values: GameData): void;
+}
+
+export const ListGames: React.FC<ListGamesProps> = (props) => {
+
+    return (
+        <div className="px-10 sm:px-24 lg:px-36 xl:px-56">
+            <ul className="flex flex-col p-4">
+                {props.games?.map((game) => (
+                    <li className="flex flex-row mb-2" onClick={() => props.launchGame(game)}>
+                        <div id='map-game' className="select-none cursor-pointer bg-map rounded-3xl flex flex-1 items-center p-4 shadow-2xl">
+                            <div className="flex flex-col border-black border-2 rounded-md w-10 h-10 bg-blue-Dark bg-opacity-75 justify-center items-center mr-4">💧</div>
+                            <div className="flex-1 pl-1 mr-16">
+                                <div className="font-iceland text-white text-xl">Partie du </div>
+                                <div className="font-iceland text-white text-sm">Tour de </div>
+                            </div>
+                            <div className="font-iceland text-white text-md">Tour {game.status}</div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
--- src/components/lobby/lobby-container.tsx
+++ src/components/lobby/lobby-container.tsx
@@ -1,8 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthenticationContext } from "../contexts/authentication-context";
 import { Link } from 'react-router-dom';
+import { ListGames } from "./list-games";
+import { GameData } from "../../util/types/data-types";
+import { useEffect } from "react";
+import { Status, useQuery } from "../../hooks/query-hooks";
+import { GamesResponse } from "../../util/types/response-types";
+import { Config } from "../../util/config";
 
 export const LobbyContainer: React.FC = () => {
+    const auth = useContext(AuthenticationContext);
+    const [games, setGames] = useState<GameData[]>(null);
+    const gamesQuery = useQuery<GamesResponse>();
+
+    useEffect(() => {
+        console.log(auth.authUser?.name);
+        switch (gamesQuery.status) {
+            case Status.INIT:
+                gamesQuery.get(`${Config}/users/${auth.authUser?.id}/games`);
+                // console.log('INIT')
+                break;
+            case Status.SUCCESS:
+                setGames(gamesQuery.response?.games);
+                // console.log('SUCCESS ' + gamesQuery.response?.games);
+                break;
+            default: break;
+        }
+    }, [gamesQuery.status]);
+
+    const handleLaunchGame = (data: GameData) => {
+        //TODO:  A rédiger
+    }
 
     return (
         <div className='bg-gradient-to-b from-secondary to-primary h-screen pt-10'>
@@ -22,15 +50,11 @@ export const LobbyContainer: React.FC = () => {
                     </div>
                 </div>
             </div>
-
-            <div className='divide-y-4 divide-gray-400 divide-opacity-50 pt-4 px-10 lg:px-20'>
-                <div className='font-iceland text-white text-md lg:text-4xl w-auto'>
-                    Vos parties en cours
-                </div>
-                {/* List of the user's current games */}
-                {
-                    <div></div>
-                }
+            <div className='pt-4 px-10 lg:px-20 font-iceland text-white text-md lg:text-4xl'>
+                Vos parties en cours
+            </div>
+            <div>
+                {games ? <ListGames games={games} launchGame={handleLaunchGame} /> : <></>}
             </div>
         </div>
     )
