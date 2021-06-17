011ffab1ea17732bb4adb47cb58adfff4514f669
Rocknologie <rocknologie@hotmail.fr>
2021-06-17 15:21:49 +0200

    Responsive list game

--- src/components/lobby/list-games.tsx
+++ src/components/lobby/list-games.tsx
@@ -8,10 +8,10 @@ export interface ListGamesProps {
 export const ListGames: React.FC<ListGamesProps> = (props) => {
 
     return (
-        <div className="px-10 sm:px-24 lg:px-36 xl:px-56">
+        <div className="px-8 sm:px-24 lg:px-36 xl:px-56">
             <ul className="flex flex-col p-4">
                 {props.games?.map((game) => (
-                    <li className="flex flex-row mb-2" onClick={() => props.launchGame(game)}>
+                    <li className=" mb-2" onClick={() => props.launchGame(game)}>
                         <div id='map-game' className="select-none cursor-pointer bg-map rounded-3xl flex flex-1 items-center p-4 shadow-2xl">
                             <div className="flex flex-col border-black border-2 rounded-md w-10 h-10 bg-blue-Dark bg-opacity-75 justify-center items-center mr-4">💧</div>
                             <div className="flex-1 pl-1 mr-16">
