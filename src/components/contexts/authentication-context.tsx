c18a67d872e654654d11545f053f594655a1ee5d
Lelberto <jsurieux@gmail.com>
2021-06-17 15:34:46 +0200

    Add auto-reconnect when the access token is expired If expired, the access token query is triggered

--- src/components/contexts/authentication-context.tsx
+++ src/components/contexts/authentication-context.tsx
@@ -3,7 +3,7 @@ import { Status, useQuery } from '../../hooks/query-hooks';
 import { Config } from '../../util/config';
 import { LocalStorageKey } from '../../util/local-storage';
 import { UserData } from '../../util/types/data-types';
-import { UserResponse } from '../../util/types/response-types';
+import { AccessTokenResponse, UserResponse } from '../../util/types/response-types';
 
 /**
  * Authentication context state.
@@ -26,6 +26,7 @@ export const AuthenticationContext = React.createContext<AuthenticationContextSt
 export const AuthenticationContextProvider: React.FC = (props) => {
   const [authUser, setAuthUser] = useState<UserData>(null);
   const userInfoQuery = useQuery<UserResponse>();
+  const accessTokenQuery = useQuery<AccessTokenResponse>();
 
   useEffect(() => {
     switch (userInfoQuery.status) {
@@ -37,10 +38,28 @@ export const AuthenticationContextProvider: React.FC = (props) => {
         console.log('User', userInfoQuery.response.user.name, 'authenticated with access token');
         break;
       case Status.ERROR:
-        console.error('Could not authenticate :', userInfoQuery.errorResponse.errors);
+        console.log(userInfoQuery.code)
+        if (userInfoQuery.code === 403) {
+          accessTokenQuery.post(`${Config.API_URL}/auth/accessToken`, { refreshToken: localStorage.getItem(LocalStorageKey.REFRESH_TOKEN) });
+        } else {
+          console.error('Could not authenticate :', userInfoQuery.errorResponse.errors);
+        }
         break;
     }
   }, [userInfoQuery.status]);
+
+  useEffect(() => {
+    switch (accessTokenQuery.status) {
+      case Status.SUCCESS:
+        const { access_token } = accessTokenQuery.response;
+        localStorage.setItem(LocalStorageKey.ACCESS_TOKEN, access_token);
+        userInfoQuery.get(`${Config.API_URL}/users/info`, { headers: { Authorization: `Bearer ${access_token}` } });
+        break;
+      case Status.ERROR:
+        console.log('Could not get new access token :', accessTokenQuery.errorResponse.errors);
+        break;
+    }
+  }, [accessTokenQuery.status]);
 
   return <AuthenticationContext.Provider value={{ authUser, setAuthUser }}>{props.children}</AuthenticationContext.Provider>
 }
