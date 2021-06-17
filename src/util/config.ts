6ed2d579d55c1e81530556f51e5c2cb407a4c7f0
Rocknologie <rocknologie@hotmail.fr>
2021-06-17 14:23:23 +0200

    Update API URL

--- src/util/config.ts
+++ src/util/config.ts
@@ -2,6 +2,6 @@
  * Global configuration enumeration.
  */
  export enum Config {
-    API_URL = 'http://localhost',
+    API_URL = 'http://localhost:80',
     WEBSOCKET_URL = 'http://localhost:8000'
 }
