30a95b15fe54bdbd25aa7d6acb4ac13bd9da3717
Rocknologie <rocknologie@hotmail.fr>
2021-06-17 15:10:16 +0200

    Responsive header

--- src/components/lobby/lobby-container.tsx
+++ src/components/lobby/lobby-container.tsx
@@ -14,15 +14,12 @@ export const LobbyContainer: React.FC = () => {
     const gamesQuery = useQuery<GamesResponse>();
 
     useEffect(() => {
-        console.log(auth.authUser?.name);
         switch (gamesQuery.status) {
             case Status.INIT:
                 gamesQuery.get(`${Config}/users/${auth.authUser?.id}/games`);
-                // console.log('INIT')
                 break;
             case Status.SUCCESS:
                 setGames(gamesQuery.response?.games);
-                // console.log('SUCCESS ' + gamesQuery.response?.games);
                 break;
             default: break;
         }
@@ -35,22 +32,22 @@ export const LobbyContainer: React.FC = () => {
     return (
         <div className='bg-gradient-to-b from-secondary to-primary h-screen pt-10'>
             {/* Top's section */}
-            <div className='bg-map w-5/6 h-3/6 mx-auto rounded-2xl' id='map'>
+            <div className='bg-map w-5/6 h-2/6 sm:h-3/6 mx-auto rounded-2xl' id='map'>
                 {/* Title */}
-                <div className='flex justify-center pt-28 lg:pt-52'>
-                    <img alt='' className='w-8/12' src='/img/shattle.png' />
+                <div className='flex justify-center pt-20 sm:pt-32 lg:pt-40'>
+                    <img alt='' className='w-7/12 sm:w-6/12 md:w-3/12 lg:w-4/12' src='/img/shattle.png' />
                 </div>
 
                 {/* Play button */}
                 <div className='flex justify-center pt-12 lg:pt-24 space-y-0'>
                     <div className='bg-white bg-opacity-50 border-2 border-white rounded-lg'>
                         <Link to='/game'>
-                            <img className='w-36 lg:w-72 p-1' src='/img/jouer.png' />
+                            <img className='w-24 sm:w-32 md:w-36 lg:w-48 p-1' src='/img/jouer.png' />
                         </Link>
                     </div>
                 </div>
             </div>
-            <div className='pt-4 px-10 lg:px-20 font-iceland text-white text-md lg:text-4xl'>
+            <div className='font-iceland pt-6 px-10 sm:px-20 lg:px-28 text-white text-lg sm:text-2xl lg:text-4xl'>
                 Vos parties en cours
             </div>
             <div>
