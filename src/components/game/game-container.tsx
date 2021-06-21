72e2c6cf99df1f93105dbc5b3a506c466e358530
Lelberto <jsurieux@gmail.com>
2021-06-21 17:12:45 +0200

    Update game container

--- src/components/game/game-container.tsx
+++ src/components/game/game-container.tsx
@@ -1,10 +1,9 @@
-import { FC, useContext, useEffect, useRef, useState } from 'react';
+import { FC, useContext, useEffect, useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Status, useQuery } from '../../hooks/query-hooks';
 import { Config } from '../../util/config';
 import { LocalStorageKey } from '../../util/local-storage';
-import { GameData } from '../../util/types/data-types';
-import { GameResponse } from '../../util/types/response-types';
+import { GameCreationResponse } from '../../util/types/response-types';
 import { Button } from '../button';
 import { AuthenticationContext } from '../contexts/authentication-context';
 import { WebsocketContext } from '../contexts/websocket-context';
@@ -12,15 +11,13 @@ import { WebsocketContext } from '../contexts/websocket-context';
 export const GameContainer: FC = () => {
   const { authUser } = useContext(AuthenticationContext);
   const { socket } = useContext(WebsocketContext);
-  const gameQuery = useQuery<GameResponse>();
-  const [game, setGame] = useState<GameData>(null);
+  const createGameQuery = useQuery<GameCreationResponse>();
   const inputRef = useRef<HTMLInputElement>();
   const history = useHistory();
 
   useEffect(() => {
     if (socket != null) {
       socket.on('game.join', ({ gameId }) => {
-        gameQuery.get(`${Config.API_URL}/games/${gameId}`);
         history.push(`/game/${gameId}`);
       });
       socket.on('game.start', console.log);
@@ -31,30 +28,27 @@ export const GameContainer: FC = () => {
         socket.off('error');
       }
     }
-  }, [socket]);
+  }, []);
 
   useEffect(() => {
-    switch (gameQuery.status) {
+    switch (createGameQuery.status) {
       case Status.SUCCESS:
-        setGame(gameQuery.response.game);
+        handleJoinGame(createGameQuery.response.code);
         break;
       case Status.ERROR:
-        console.error(gameQuery.errorResponse.errors);
+        console.error(createGameQuery.errorResponse.errors);
         break;
     }
-  }, [gameQuery.status]);
-
-  const handleJoinGame = () => {
-    socket.emit('game.join', { code: inputRef.current.value, accessToken: localStorage.getItem(LocalStorageKey.ACCESS_TOKEN) });
-  }
+  });
 
   const handleCreateGame = () => {
     if (authUser) {
-      const body = {
-        player: authUser.id,
-      }
-      gameQuery.post(`${Config.API_URL}/games`, body);
+      createGameQuery.post(`${Config.API_URL}/games`, { player: authUser.id });
     }
+  }
+
+  const handleJoinGame = (code: string) => {
+    socket.emit('game.join', { code, accessToken: localStorage.getItem(LocalStorageKey.ACCESS_TOKEN) });
   }
 
   return (
@@ -70,7 +64,7 @@ export const GameContainer: FC = () => {
             <input className='border-2 border-primary rounded-lg' ref={inputRef} type="text" placeholder="Game code" />
           </div>
           <div className='bg-white bg-opacity-25 border-2 border-white rounded-lg mx-auto flex justify-center w-6/12 '>
-            <Button className='font-iceland text-3xl text-white' onClick={handleJoinGame}>Rejoindre une partie</Button>
+            <Button className='font-iceland text-3xl text-white' onClick={() => handleJoinGame(inputRef.current.value)}>Rejoindre une partie</Button>
           </div>
         </div>
       </div>
