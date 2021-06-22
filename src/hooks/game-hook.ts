8c7f9b97901afe14dd455232926e18c695f9003e
Lelberto <jsurieux@gmail.com>
2021-06-22 17:28:19 +0200

    Add game hook

---
+++ src/hooks/game-hook.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import { Config } from '../util/config';
+import { GameData } from '../util/types/data-types';
+import { GameResponse } from '../util/types/response-types'
+import { Status, useQuery } from './query-hooks'
+
+/**
+ * Game hook.
+ * 
+ * This hook is used to keep a game state and refresh it.
+ * 
+ * @param gameId Game ID
+ * @returns [Game, refresh function]
+ */
+export const useGame = (gameId: string): [GameData, () => void] => {
+  const gameQuery = useQuery<GameResponse>();
+  const [game, setGame] = useState<GameData>(null);
+
+  useEffect(() => {
+    switch (gameQuery.status) {
+      case Status.INIT:
+        refresh();
+        break;
+      case Status.SUCCESS:
+        setGame(gameQuery.response.game);
+        break;
+      case Status.ERROR:
+        console.error(gameQuery.errorResponse.errors);
+        break;
+    }
+  }, [gameQuery.status]);
+
+  function refresh() {
+    gameQuery.get(`${Config.API_URL}/games/${gameId}`);
+  }
+
+  return [game, refresh];
+}
