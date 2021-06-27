2bef73a496ef206b37ac248fde648e798040e4cc
Lelberto <jsurieux@gmail.com>
2021-06-27 21:09:50 +0200

    Add end round logic

--- src/components/game/playing-container.tsx
+++ src/components/game/playing-container.tsx
@@ -1,13 +1,38 @@
-import { FC } from 'react';
-import { GameData } from '../../util/types/data-types';
+import _ from 'lodash';
+import { FC, useContext, useMemo, useState } from 'react';
+import { ActionData, ActionType, GameData, TileData } from '../../util/types/data-types';
+import { Button } from '../button';
+import { AuthenticationContext } from '../contexts/authentication-context';
 import { MapCanvas } from './map-canvas';
 
 export interface PlayingContainer {
   game: GameData;
+  onEndRound: (actions: ActionData[]) => void;
 }
 
-export const PlayingContainer: FC<PlayingContainer> = ({ game }) => {
+export const PlayingContainer: FC<PlayingContainer> = ({ game, onEndRound }) => {
+  const { authUser } = useContext(AuthenticationContext);
+  const [actions, setActions] = useState<ActionData[]>([]);
+
+  const player = useMemo(() => {
+    if (authUser) {
+      return game.players.find(player => player.user.id === authUser.id);
+    }
+    return null;
+  }, [authUser]);
+
+  const handleMove = (tile: TileData) => {
+    setActions((oldState => [...oldState, {
+      type: ActionType.MOVE,
+      to: tile
+    }]));
+  }
+
   return (
-    <MapCanvas game={game} width={game.map.config?.width * game.map.config.tilewidth} height={game.map.config.height * game.map.config.tileheight} />
+    <>
+      <MapCanvas game={game} width={game.map.config?.width * game.map.config.tilewidth} height={game.map.config.height * game.map.config.tileheight} />
+      <Button onClick={() => onEndRound(actions)}>Terminer le tour</Button>
+      <Button onClick={() => handleMove(game.map.mapTiles[_.random(0, game.map.mapTiles.length - 1)])}>Move (WIP)</Button>
+    </>
   );
 }
